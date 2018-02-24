32a85f7984de09e0e2629ff7803df68d480ad9d2
John Iyere <john.iyere@student.manchester.ac.uk>
2018-02-24 17:02:56 +0000

    mouse hover resizes circles in range

--- foundation/canvas.js
+++ foundation/canvas.js
@@ -37,6 +37,22 @@ let context = canvas.getContext('2d');
 //     context.strokeStyle = getRandomColor();
 //     context.stroke();
 // }
+let mouse = {
+    x: undefined,
+    y: undefined
+}
+
+window.addEventListener('mousemove', (event) => {
+    mouse.x = event.x;
+    mouse.y = event.y;
+})
+
+window.addEventListener('resize', (event) => {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init();
+
+})
 
 function getRandomColor() {
     let chars = '0123456789ABCDEF';
@@ -48,22 +64,23 @@ function getRandomColor() {
 }
 
 class Circle {
+
     constructor(x, y, dx, dy, radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
+        this.minRadius = radius;
         this.dx = dx;
         this.dy = dy;
         this.outline = getRandomColor();
-        this.fill = getRandomColor();
+        this.fill = Circle.colorArray()[Math.floor(Math.random() * Circle.colorArray().length)];
     }
 
     draw() {
         context.beginPath();
         context.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-        context.strokeStyle = getRandomColor();
         context.stroke();
-        context.fillStyle = getRandomColor();
+        context.fillStyle = this.fill;
         context.fill()
     }
 
@@ -77,29 +94,64 @@ class Circle {
         }
         this.x += this.dx;
         this.y += this.dy;
+
+        // interactivity
+        if ( this.getDistanceFromMouse() < 50 && this.radius < Circle.maxRadius()) {
+            this.radius += 1 
+        } else if (this.radius > this.minRadius) {
+            this.radius -= 1 % this.radius
+        }
+
         this.draw()
+    }
+
+    getDistanceFromMouse() {
+        let x = Math.pow(mouse.x - this.x, 2);
+        let y = Math.pow(mouse.y - this.y, 2);
+
+        return Math.sqrt(x + y)
+    }
+
+    static maxRadius() {
+        return 40;
+    }
+
+
+    static colorArray() {
+        return [
+            '#F3021A',
+            '#010326',
+            '#05D7C6',
+            '#F5B603',
+            '#F38806'
+        ]
     }
 }
 
 let circles = [];
 
-for (let i = 0; i < 100; i++) {
-    let radius = 30;
 
-    let x = Math.random() * (innerWidth - radius * 2) + radius;
-    let y = Math.random() * (innerHeight - radius * 2) + radius;
-    let dx = (Math.random() - 0.5) * 8;
-    let dy = (Math.random() - 0.5) * 8;
-    circles.push(new Circle(x, y, dx, dy, radius))
+function init() {
+    circles = []
+
+    for (let i = 0; i < 1000; i++) {
+        let radius = Math.random() * 3 + 1;
+
+        let x = Math.random() * (innerWidth - radius * 2) + radius;
+        let y = Math.random() * (innerHeight - radius * 2) + radius;
+        let dx = (Math.random() - 0.5) * 1;
+        let dy = (Math.random() - 0.5) * 1;
+        circles.push(new Circle(x, y, dx, dy, radius))
+    }
 }
 
 function animate() {
     requestAnimationFrame(animate);
     context.clearRect(0, 0 ,innerWidth, innerHeight);
-
     circles.forEach(circle => {
         circle.update()
     });
 }
 
+init()
 animate()
